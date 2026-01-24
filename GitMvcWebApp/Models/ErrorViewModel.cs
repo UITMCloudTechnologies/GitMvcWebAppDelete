c10a0eea74fe2018d8af2c7a778e107d5c36f7c4
Marcin Jagieła <mjagiela@wsiz.edu.pl>
2026-01-24 17:42:00 +0100

    Add project files.

---
+++ GitMvcWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitMvcWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
